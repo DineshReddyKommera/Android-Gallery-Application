3d2ef0223e386eca7cc73f04b2a0b5a275e29923
Abhinay <abhinay7777@gmail.com>
2020-06-10 01:41:38 -0700

    fix:Navigation to previous image animation issue

--- main/java/com/example/photoalbum/MainActivity.java
+++ main/java/com/example/photoalbum/MainActivity.java
@@ -11,28 +11,35 @@ public class MainActivity extends AppCompatActivity implements OnButtonPressList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadFragment(R.drawable.image1); // fragment is loaded with first image from the list
+        loadFragment(R.drawable.image1,"Next"); // fragment is loaded with first image from the list
     }
 
     /**
      * Implementation of abstract methods from {@link OnButtonPressListener} interface
      * @param imageID
+     * @param direction
      */
     @Override
-    public void onButtonPressed(Integer imageID) {
-        loadFragment(imageID);
+    public void onButtonPressed(Integer imageID,String direction) {
+        loadFragment(imageID,direction);
     }
 
     /**
      * Fragment is loaded with default
      * @param imageID or imageID send from {@link ImageFragment}
+     * @param direction specifies in which direction image navigation
+     * is taking place
      */
-    private void loadFragment(Integer imageID){
+    private void loadFragment(Integer imageID,String direction){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
         ImageFragment imageFragment=new ImageFragment(imageID);
+        if(direction.equals("Next"))
         fragmentTransaction.setCustomAnimations(R.anim.enter, R.anim.exit)
                 .replace(R.id.fragment_container,imageFragment);
+        else
+            fragmentTransaction.setCustomAnimations(R.anim.pop_enter, R.anim.pop_exit)
+                    .replace(R.id.fragment_container,imageFragment);
         fragmentTransaction.commit();
     }
 }
